b164f829942856691367d2716efc8ee42e3ec734
contigen <69436315+contigen@users.noreply.github.com>
2022-09-29 00:40:01 +0100

    added a new helper method

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,8 @@ export function checkForValue(query, Arr) {
   return Arr.includes(query);
 }
 export function findValueIndex(query, Arr) {
-  return Arr.findIndex((el) => query === el);
+  return Arr.findIndex((val) => query === val);
+}
+export function findElement(query, Arr) {
+  return Arr.find((el) => query === el.innerText);
 }
