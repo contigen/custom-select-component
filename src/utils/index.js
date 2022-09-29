b46515d7ae28de411a2d2d21efbceb12512136a8
contigen <69436315+contigen@users.noreply.github.com>
2022-09-29 02:30:58 +0100

    chore: added new helper functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,3 +7,19 @@ export function findValueIndex(query, Arr) {
 export function findElement(query, Arr) {
   return Arr.find((el) => query === el.innerText);
 }
+export const addSelectedAttr = (value, Arr) => {
+  const targetElement = findElement(value, Arr);
+  targetElement.ariaSelected = true;
+};
+export const addSingleSelectedAttr = (value, Arr) => {
+  // for single selection mode, need to reset the aria-selected attribute of previous selected item elements to false, and set true for the latest selected item element
+  resetSelectedAttr(Arr);
+  addSelectedAttr(value, Arr);
+};
+export const removeSelectedAttr = (value, Arr) => {
+  const targetElement = findElement(value, Arr);
+  targetElement.ariaSelected = false;
+};
+export const resetSelectedAttr = (Arr) => {
+  Arr.forEach((el) => (el.ariaSelected = false));
+};
