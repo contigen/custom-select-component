ecf27449d336bc329667dc4e9bec2bf4d0ab0649
contigen <69436315+contigen@users.noreply.github.com>
2022-09-29 03:20:08 +0100

    deleted unneeded code - react-router

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<App />} />
-      </Routes>
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
