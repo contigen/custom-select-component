b5dd8d0ae7c413ac4c3e9d1de39f16d07b063b89
contigen <69436315+contigen@users.noreply.github.com>
2023-02-13 23:30:26 +0100

    Add button elements to modify component state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import "./App.css";
+import { useState } from "react";
 import Select from "./components/select";
+import { Button } from "./components/button";
+
 function App() {
-  return <Select multiple defaultArr={[1, 2, 3, 4]} />;
+  const [selectConfig, setSelectConfig] = useState({
+    autoselect: false,
+    multiple: true,
+  });
+  const handleClick = ({ target }) => {
+    const stateConfigID = target.dataset.state_id;
+    setSelectConfig((prevState) => ({
+      ...prevState,
+      [stateConfigID]: !prevState[stateConfigID],
+    }));
+  };
+  return (
+    <>
+      <h1 className="select__first-text">A custom select component</h1>
+      <div>
+        <Button data-state_id="autoselect" onClick={handleClick}>
+          {selectConfig.autoselect ? `Disable` : `Enable`} autoselect mode
+        </Button>{" "}
+        <Button data-state_id="multiple" onClick={handleClick}>
+          Enable {selectConfig.multiple ? `single` : `multiple`} selection mode
+        </Button>
+      </div>
+      <br />
+      <Select
+        autoselect={selectConfig.autoselect}
+        multiple={selectConfig.multiple}
+        defaultArr={[1, 2, 3, 4]}
+      />
+    </>
+  );
 }
 export default App;
